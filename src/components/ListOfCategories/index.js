9672dfd39ad98545c389e6fd2c45d3d35ae4a65e
fermin <fermartorr01@gmail.com>
2021-01-15 17:01:06 +0100

    CREANDO EL RESTO Y COMENZANDO CON HOOKS
    - Usamos info real para las categorías
    - Creo componente PhotoCard y ListOfPhotoCard + Icono con React-Icons
    - Pasamos Logo svg a componente
    - Creamos animaciones
    _ Hooks: Aplico useState y useEffect para hacer fetch de la API
    - useEffect para crear el submenu fixed y limpio el evento después

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,14 +1,43 @@
-import React from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Category } from '../Category'
 
 import { List, Item } from './styles'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+
+  useEffect(function () {
+    window.fetch('https://petgram-server-fermin-3iamgw65r.vercel.app/categories')
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
+
+  useEffect(function () {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
-        [1, 2, 3, 4].map(category => <Item key={category}><Category /></Item>)
+        categories.map(category => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
--- src/components/ListOfCategories/styles.js
+++ src/components/ListOfCategories/styles.js
@@ -1,9 +1,24 @@
 import styled from 'styled-components'
+import { appearFromTop } from '../../styles/animation'
 
 export const List = styled.ul`
   display: flex;
   overflow: scroll;
   width: 100%;
+  &.fixed {
+    background: white;
+    border-radius: 60px;
+    box-shadow: 0 0 20px rgba(0, 0, 0, .3);
+    left: 0;
+    margin: 0 auto;
+    max-width: 400px;
+    padding: 5px;
+    position: fixed;
+    right: 0;
+    top: -20px;
+    transform: scale(.5);
+    z-index: 1;
+  }
 `
 export const Item = styled.li`
   padding: 0 8px;
