af0b0afe8de80192fd5e1818181c8a2bed50a42b
fermin <fermartorr01@gmail.com>
2021-01-25 17:12:49 +0100

    - Refactorizo y uso las variables loading y error - Uso las mutaciones con los likes

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,16 +1,17 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react'
-import { Article, ImgWrapper, Img, Button } from './styles'
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { Article, ImgWrapper, Img } from './styles'
+
 import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
+
+import { FavButton } from '../FavButton'
+import { ToggleLikeMutation } from '../../container/ToogleLikeMutation'
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1452857297128-d9c29adba80b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
   const key = `like-${id} `
   const [liked, setLiked] = useLocalStorage(key, false)
-
-  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={element}>
@@ -22,9 +23,22 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button onClick={() => setLiked(!liked)}>
-            <Icon size='32px' /> {likes} likes!
-          </Button>
+          <ToggleLikeMutation>
+            {
+              (toogleLike) => {
+                const handleFavClick = () => {
+                  !liked && toogleLike({
+                    variables: {
+                      input: { id }
+                    }
+                  })
+                  setLiked(!liked)
+                }
+                return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
+              }
+            }
+          </ToggleLikeMutation>
+
                 </>
       }
     </Article>
