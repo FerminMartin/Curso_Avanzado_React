8f8cfec36afca9c0e0b8033fa25515faa74318c5
fermin <fermartorr01@gmail.com>
2021-01-26 07:17:33 +0100

    - Uso Link para evitar recargar la página - Creo la página de Detail

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -6,6 +6,8 @@ import { useNearScreen } from '../../hooks/useNearScreen'
 
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToogleLikeMutation'
+import { Link } from '@reach/router'
+
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1452857297128-d9c29adba80b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
@@ -17,11 +19,11 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
     <Article ref={element}>
       {
         show && <>
-          <a href={`/?detail=${id}`}>
+          <Link to={`/detail/${id}`}>
             <ImgWrapper>
               <Img src={src} />
             </ImgWrapper>
-          </a>
+          </Link>
 
           <ToggleLikeMutation>
             {
