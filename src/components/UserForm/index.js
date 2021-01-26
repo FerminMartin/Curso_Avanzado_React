1de517e2dc40a31ddbb720ae1abaa7fef5fd24b0
fermin <fermartorr01@gmail.com>
2021-01-26 22:48:26 +0100

    Mutaciones para el registro - Guardamos la info del usuario gracias a una mutación de la base de datos de Graphql

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -5,10 +5,19 @@ import { Form, Input, Button, Title } from './styles'
 export const UserForm = ({ onSubmit, title }) => {
   const email = useInputValue('')
   const password = useInputValue('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    onSubmit({
+      email: email.value,
+      password: password.value
+    })
+  }
+
   return (
     <>
       <Title>{title}</Title>
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={handleSubmit}>
         <Input placeholder='Email' {...email} />
         <Input placeholder='Password' type='password' {...password} />
         <Button>{title}</Button>
