89665bfb7df07cd744e62e2788283a2ef9851a72
fermin <fermartorr01@gmail.com>
2021-01-27 13:13:26 +0100

    - Mutaciones para iniciar sesión - Manejar el estado de Loading y Error

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
+import { LoginMutation } from '../container/LoginMutation'
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
@@ -24,7 +25,21 @@ export const NotRegisteredUser = () => (
               }
             }
             </RegisterMutation>
-            <UserForm title='Iniciar sesión' onSubmit={activateAuth} />
+            <LoginMutation>
+              {
+                (login, { data, loading, error }) => {
+                  const onSubmit = ({ email, password }) => {
+                    const input = { email, password }
+                    const variables = { input }
+                    login({ variables }).then(activateAuth)
+                  }
+
+                  const errorMsg = error && '🤖 La contraseña no es correcta o el usuario no existe.'
+
+                  return <UserForm disabled={loading} error={errorMsg} title='Iniciar sesión' onSubmit={onSubmit} />
+                }
+              }
+            </LoginMutation>
           </>
         )
       }
