f1027dea19b068d20dbbb58d46a6a30d681aaf18
fermin <fermartorr01@gmail.com>
2021-01-28 01:11:46 +0100

    7 - 8 Hacer like como usuario registrado
    -Hacer peticiones de usuario registrado usando el token

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,7 +1,6 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react'
 import { Article, ImgWrapper, Img } from './styles'
 
-import { useLocalStorage } from '../../hooks/useLocalStorage'
 import { useNearScreen } from '../../hooks/useNearScreen'
 
 import { FavButton } from '../FavButton'
@@ -10,10 +9,8 @@ import { Link } from '@reach/router'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1452857297128-d9c29adba80b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
-export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
   const [show, element] = useNearScreen()
-  const key = `like-${id} `
-  const [liked, setLiked] = useLocalStorage(key, false)
 
   return (
     <Article ref={element}>
@@ -29,19 +26,18 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             {
               (toogleLike) => {
                 const handleFavClick = () => {
-                  !liked && toogleLike({
+                  toogleLike({
                     variables: {
                       input: { id }
                     }
                   })
-                  setLiked(!liked)
                 }
                 return <FavButton liked={liked} likes={likes} onClick={handleFavClick} />
               }
             }
           </ToggleLikeMutation>
 
-                </>
+        </>
       }
     </Article>
   )
