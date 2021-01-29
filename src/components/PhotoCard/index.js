c3591464c7a943f25a5b4067379acab6eb864c8d
fermin <fermartorr01@gmail.com>
2021-01-29 20:40:25 +0100

    8.5 Usando PropTypes para validar las props
    - Aplico en dif componentes en valores bool, number, string, func, array y node
    - Array y node son valores un poco más complicados

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -6,6 +6,7 @@ import { useNearScreen } from '../../hooks/useNearScreen'
 import { FavButton } from '../FavButton'
 import { ToggleLikeMutation } from '../../container/ToogleLikeMutation'
 import { Link } from '@reach/router'
+import PropTypes from 'prop-types'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1452857297128-d9c29adba80b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
@@ -37,8 +38,25 @@ export const PhotoCard = ({ id, liked, likes = 0, src = DEFAULT_IMAGE }) => {
             }
           </ToggleLikeMutation>
 
-        </>
+                </>
       }
     </Article>
   )
+}
+
+PhotoCard.proptypes = {
+  id: PropTypes.string.isRequired,
+  liked: PropTypes.bool.isRequired,
+  scr: PropTypes.string.isRequired,
+  likes: function (props, propName, componentName) {
+    const propValue = props[propName]
+
+    if (propValue === undefined) {
+      return new Error(`${propName} value must be defined`)
+    }
+
+    if (propValue < 0) {
+      return new Error(`${propName} value must be greater than 0`)
+    }
+  }
 }
